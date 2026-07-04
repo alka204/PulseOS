81c2eb122956e0742935221832bb7a969d6cb307
Alka Kumari <alka.kumari3289@gmail.com>
2026-07-04 18:28:53 +0530

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,13 @@ const app = express();
 // --- Middleware ---
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://pulse-os-six.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
 
